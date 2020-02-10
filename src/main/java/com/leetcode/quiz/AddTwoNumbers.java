f9cafffca35e80e3cc8ebad9bd7643f9454bd39f
alvenchen999 <alven.chen@aliyun.com>
2020-02-10 17:20:15 +0800

    Add two numbers

--- src/main/java/com/leetcode/quiz/AddTwoNumbers.java
+++ src/main/java/com/leetcode/quiz/AddTwoNumbers.java
@@ -15,4 +15,41 @@ Explanation: 342 + 465 = 807.
 	 */
 
 public class AddTwoNumbers {
+
+    public IntNode addTwoNumbers(IntNode a, IntNode b){
+        IntNode res = new IntNode(0);
+        IntNode p = a, q = b, curr = res;
+        int increase = 0;
+        while (p != null || q != null){
+            int t1 = (p != null)? p.value:0;
+            int t2 = (q != null)? q.value:0;
+            int sum = t1 + t2 + increase;
+            increase = sum/10;
+            curr.next = new IntNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (increase > 0){
+            curr.next = new IntNode(increase);
+        }
+        return res.next;
+    }
+
+
+    public static void main(String[] args){
+        AddTwoNumbers res = new AddTwoNumbers();
+        IntNode l1 = new IntNode(2);
+        IntNode l2 = new IntNode(4);
+        IntNode l3 = new IntNode(3);
+        IntNode ll1 = new IntNode(5);
+        IntNode ll2 = new IntNode(6);
+        IntNode ll3 = new IntNode(9);
+        IntNode ll4 = new IntNode(1);
+
+        l1.next = l2; l2.next = l3;
+        ll1.next = ll2; ll2.next = ll3; ll3.next = ll4;
+        IntNode final_res = res.addTwoNumbers(l1, ll1);
+        IntNode.printNodeList(final_res);
+    }
 }
--- src/main/java/com/leetcode/quiz/DataModel.java
+++ src/main/java/com/leetcode/quiz/DataModel.java
@@ -10,4 +10,12 @@ class IntNode {
     IntNode(int num) {
         value = num;
     }
+
+    static void printNodeList(IntNode a){
+        while(a != null){
+            System.out.printf("%d->", a.value);
+            a = a.next;
+        }
+        System.out.print("NULL \n");
+    }
 }
